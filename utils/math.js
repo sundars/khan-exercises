862c8427ba3f0cd04df36f34dfe8181fa3acdd0a
Omar Rizwan <omar.rizwan@gmail.com>
2011-05-17 13:55:09 -0700

    Not-yet-working partial port of functions_3 for development purposes.

--- utils/math.js
+++ utils/math.js
@@ -8,10 +8,28 @@ $.extend(KhanUtil, {
 	randRange: function( min, max ) {
 		return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 	},
+
+	// Get a random nonzero integer between min and max, inclusive
+	randRangeNonZero: function( min, max ) {
+		var num;
+		do {
+			num = this.randRange( min, max );
+		} while ( num === 0 );
+		return num;
+	},
 	
 	// Returns a random member of the given array
 	randFromArray: function( arr ) {
 		return arr[ this.rand( arr.length - 1 ) ];
+	},
+
+	// Returns a random member of arr which is not in narr
+	randFromArrayNotIn: function( arr, narr ) {
+		var member;
+		do {
+			member = this.randFromArray(arr);
+		} while ( $.inArray( member, narr ) != -1 );
+		return member;
 	},
 
 	// Round a number to a certain number of decimal places
